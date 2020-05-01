bb93b373cb52f006757a1e06d70939c8f89240c1
b-arvo <brennanarvo@gmail.com>
2020-05-01 17:08:02 -0400

    Updates

---
+++ precache-manifest.8948ea031601480ea0419c5cc57018d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2a34ff0d5e2c22dc03a53372e2f7444",
+    "url": "/MeMoji/index.html"
+  },
+  {
+    "revision": "b48a710f40f1f5e90ef9",
+    "url": "/MeMoji/static/css/main.4eb516cb.chunk.css"
+  },
+  {
+    "revision": "5e42b3e5914f5622b458",
+    "url": "/MeMoji/static/js/2.c552b942.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MeMoji/static/js/2.c552b942.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b48a710f40f1f5e90ef9",
+    "url": "/MeMoji/static/js/main.6bb252ff.chunk.js"
+  },
+  {
+    "revision": "6786f76554b9db14acd2",
+    "url": "/MeMoji/static/js/runtime-main.b29c9a40.js"
+  }
+]);
